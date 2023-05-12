c3280745a4fe0d290b3fed84571c8efa701a3bb1
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-05-12 14:38:31 -0300

    req 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,23 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products):
+        fabrication = [item["data_de_fabricacao"]
+                       for item in products]
+
+        validate = [item["data_de_validade"]
+                    for item in products
+                    if item["data_de_validade"] >= datetime
+                    .now().strftime("%Y-%m-%d")]
+
+        company = Counter([item["nome_da_empresa"]
+                           for item in products]).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {min(fabrication)}\n"
+            f"Data de validade mais próxima: {min(validate)}\n"
+            f"Empresa com mais produtos: {company}"
+        )
