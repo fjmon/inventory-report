749128ddb1f7405f606ce58a8d5de17800cf8b41
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-05-12 19:13:54 -0300

    req 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,13 @@
+from .simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(product):
+        list = ""
+        simple_report = f"{SimpleReport.generate(product)}\n"
+        companies = Counter([item["nome_da_empresa"] for item in product])
+        for item in companies.items():
+            list += f"- {item[0]}: {item[1]}\n"
+        return f"{simple_report}Produtos estocados por empresa:\n{list}"
