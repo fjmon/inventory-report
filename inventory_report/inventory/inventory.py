9f264c2f80310d55a9ac94385ee9270dc226b542
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-05-14 18:27:56 -0300

    req 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,9 +7,16 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @staticmethod
     def import_data(file_path: str, report_type: str) -> str:
-        with open(file_path, newline='') as arquivo:
-            reader = csv.DictReader(arquivo)
-            stock = [row for row in reader]
+        extension = file_path.split(".")[-1]
+        if extension == "csv":
+            with open(file_path, newline='') as arquivo:
+                reader = csv.DictReader(arquivo)
+                stock = [row for row in reader]
+        elif extension == "json":
+            with open(file_path) as file:
+                stock = json.load(file)
+        else:
+            raise ValueError("Extensão inválida")
 
         if report_type == "simples":
             report = SimpleReport.generate(stock)
